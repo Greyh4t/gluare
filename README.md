136f2486cb19e168f77dc8a1997d6613157b9f51
lgq12172009 <lgq.1217@163.com>
2017-11-09 12:07:35 +0800

    add new function

--- README.md
+++ README.md
@@ -17,37 +17,99 @@ func main() {
 	L.PreloadModule("re", gluare.Loader)
 	err := L.DoString(`
 		local re=require("re")
-		local re1 = re.match("\\d{3}", "aaa000bbb")
-		print(re1)
-		p, err = re.compile("bbb(\\d{3})")
+		local pattern = "bbb(\\d{3})"
+		local str = "aaa000bbb111aaa222bbb333ccc444"
+
+		print("[+] re.match")
+		local r, err = re.match(pattern, str)
+		print(r, err)
+
+		print("[+] re.find")
+		local r, err = re.find(pattern, str)
+		print(r, err)
+
+		print("[+] re.findindex")
+		local r, err = re.findindex(pattern, str)
+		print(r[1], r[2], err)
+
+		print("[+] re.findsub")
+		local r, err = re.findsub(pattern, str)
+		print(r[1], err)
+
+		print("[+] re.findall")
+		local r, err = re.findall(pattern, str)
+		print(r[1], err)
+
+		print("[+] re.findallindex")
+		local r, err = re.findallindex(pattern, str)
+		print(r[1][1], r[1][2], r[2][1], r[1][2], err)
+
+		print("[+] re.findallsub")
+		local r, err = re.findallsub(pattern, str)
+		print(r[1][1], r[2][1], err)
+
+		print("[+] re.split")
+		local r, err = re.split(pattern, str)
+		print(r[1], r[2], err)
+
+		print("[+] re.split")
+		local r, err = re.split(pattern, str, 2)
+		print(r[1], r[2], err)
+
+		print("[+] re.replace")
+		local r, err = re.replace(pattern, str, "---")
+		print(r, err)
+
+		print("[+] re.compile")
+		p, err = re.compile(pattern)
 		if err~=nil then
 			print(err)
 			return
 		end
-		local x = "aaa000bbb111aaa222bbb333ccc444"
-		local re2 = p:find(x)
-		print(re2)
-		local re3 = p:findindex(x)
-		print(re3[1],re3[2])
-		local re4 = p:findsub(x)
-		print(re4[1])
-		local re5 = p:findall(x)
-		print(re5[1],re5[2])
-		local re6 = p:findall(x, 1)
-		print(re6[1])
-		local re7 = p:findallindex(x, 1)
-		print(re7[1][1], re7[1][2])
-		local re8 = p:findallindex(x)
-		print(re8[1][1], re8[1][2])
-		print(re8[2][1], re8[1][2])
-		local re9 = p:findallsub(x)
-		print(re9[1][1], re9[2][1])
-		local re10 = p:split(x)
-		print(re10[1], re10[2], re10[3])
-		local re11 = p:split(x, 2)
-		print(re11[1], re11[2])
-		local re12 = p:replace(x, "---")
-		print(re12)`,
+
+		print("[+] p:find")
+		local r = p:find(str)
+		print(r)
+
+		print("[+] p:findindex")
+		local r = p:findindex(str)
+		print(r[1],r[2])
+
+		print("[+] p:findsub")
+		local r = p:findsub(str)
+		print(r[1])
+
+		print("[+] p:findall")
+		local r = p:findall(str)
+		print(r[1],r[2])
+
+		print("[+] p:findall")
+		local r = p:findall(str, 1)
+		print(r[1])
+
+		print("[+] p:findallindex")
+		local r = p:findallindex(str, 1)
+		print(r[1][1], r[1][2])
+
+		print("[+] p:findallindex")
+		local r = p:findallindex(str)
+		print(r[1][1], r[1][2], r[2][1], r[1][2])
+
+		print("[+] p:findallsub")
+		local r = p:findallsub(str)
+		print(r[1][1], r[2][1])
+
+		print("[+] p:split")
+		local r = p:split(str)
+		print(r[1], r[2], r[3])
+
+		print("[+] p:split")
+		local r = p:split(str, 2)
+		print(r[1], r[2])
+
+		print("[+] p:replace")
+		local r = p:replace(str, "---")
+		print(r)`,
 	)
 	if err != nil {
 		fmt.Println(err)
--- gluare.go
+++ gluare.go
@@ -8,12 +8,7 @@ import (
 
 func Loader(L *lua.LState) int {
 	re := L.SetFuncs(L.NewTable(), map[string]lua.LGFunction{
-		"match":   match,
-		"compile": compile,
-	})
-
-	mt := L.NewTypeMetatable("Regexp")
-	L.SetField(mt, "__index", L.SetFuncs(L.NewTable(), map[string]lua.LGFunction{
+		"match":        match,
 		"find":         find,
 		"findindex":    findIndex,
 		"findsub":      findSubmatch,
@@ -22,7 +17,20 @@ func Loader(L *lua.LState) int {
 		"findallsub":   findAllSubmatch,
 		"replace":      replace,
 		"split":        split,
+		"compile":      compile,
+	})
+
+	mt := L.NewTypeMetatable("Regexp")
+	L.SetField(mt, "__index", L.SetFuncs(L.NewTable(), map[string]lua.LGFunction{
 		"match":        pMatch,
+		"find":         pFind,
+		"findindex":    pFindIndex,
+		"findsub":      pFindSubmatch,
+		"findall":      pFindAll,
+		"findallindex": pFindAllIndex,
+		"findallsub":   pFindAllSubmatch,
+		"replace":      pReplace,
+		"split":        pSplit,
 	}))
 	L.SetField(re, "Regexp", mt)
 
@@ -31,9 +39,9 @@ func Loader(L *lua.LState) int {
 }
 
 func match(L *lua.LState) int {
-	pattern := L.CheckString(1)
+	p := L.CheckString(1)
 	s := L.CheckString(2)
-	matched, err := regexp.MatchString(pattern, s)
+	matched, err := regexp.MatchString(p, s)
 	L.Push(lua.LBool(matched))
 	if err != nil {
 		L.Push(lua.LString(err.Error()))
@@ -42,16 +50,193 @@ func match(L *lua.LState) int {
 	return 1
 }
 
+func find(L *lua.LState) int {
+	p := L.CheckString(1)
+	s := L.CheckString(2)
+	pattern, err := regexp.Compile(p)
+	if err != nil {
+		L.Push(lua.LNil)
+		L.Push(lua.LString(err.Error()))
+		return 2
+	}
+	L.Push(lua.LString(pattern.FindString(s)))
+	return 1
+}
+
+func findIndex(L *lua.LState) int {
+	p := L.CheckString(1)
+	s := L.CheckString(2)
+
+	pattern, err := regexp.Compile(p)
+	if err != nil {
+		L.Push(lua.LNil)
+		L.Push(lua.LString(err.Error()))
+		return 2
+	}
+
+	tmp := pattern.FindStringIndex(s)
+	loc := L.NewTable()
+	if tmp != nil {
+		loc.Append(lua.LNumber(tmp[0]))
+		loc.Append(lua.LNumber(tmp[1]))
+	}
+	L.Push(loc)
+	return 1
+}
+
+func findSubmatch(L *lua.LState) int {
+	p := L.CheckString(1)
+	s := L.CheckString(2)
+
+	pattern, err := regexp.Compile(p)
+	if err != nil {
+		L.Push(lua.LNil)
+		L.Push(lua.LString(err.Error()))
+		return 2
+	}
+
+	tmp := pattern.FindStringSubmatch(s)
+	matched := L.NewTable()
+	if tmp != nil {
+		for i, m := range tmp {
+			if i == 0 {
+				continue
+			}
+			matched.Append(lua.LString(m))
+		}
+	}
+	L.Push(matched)
+	return 1
+}
+
+func findAll(L *lua.LState) int {
+	p := L.CheckString(1)
+	s := L.CheckString(2)
+	n := L.ToInt(3)
+	if n <= 0 {
+		n = -1
+	}
+
+	pattern, err := regexp.Compile(p)
+	if err != nil {
+		L.Push(lua.LNil)
+		L.Push(lua.LString(err.Error()))
+		return 2
+	}
+
+	tmp := pattern.FindAllString(s, n)
+	matched := L.NewTable()
+	for _, m := range tmp {
+		matched.Append(lua.LString(m))
+	}
+	L.Push(matched)
+	return 1
+}
+
+func findAllIndex(L *lua.LState) int {
+	p := L.CheckString(1)
+	s := L.CheckString(2)
+	n := L.ToInt(3)
+	if n <= 0 {
+		n = -1
+	}
+
+	pattern, err := regexp.Compile(p)
+	if err != nil {
+		L.Push(lua.LNil)
+		L.Push(lua.LString(err.Error()))
+		return 2
+	}
+
+	tmp := pattern.FindAllStringIndex(s, n)
+	locs := L.NewTable()
+	for _, m := range tmp {
+		loc := L.NewTable()
+		loc.Append(lua.LNumber(m[0]))
+		loc.Append(lua.LNumber(m[1]))
+		locs.Append(loc)
+	}
+	L.Push(locs)
+	return 1
+}
+
+func findAllSubmatch(L *lua.LState) int {
+	p := L.CheckString(1)
+	s := L.CheckString(2)
+	n := L.ToInt(3)
+	if n <= 0 {
+		n = -1
+	}
+
+	pattern, err := regexp.Compile(p)
+	if err != nil {
+		L.Push(lua.LNil)
+		L.Push(lua.LString(err.Error()))
+		return 2
+	}
+
+	tmp := pattern.FindAllStringSubmatch(s, n)
+	matched := L.NewTable()
+	for _, each := range tmp {
+		match := L.NewTable()
+		for i, m := range each {
+			if i == 0 {
+				continue
+			}
+			match.Append(lua.LString(m))
+		}
+		matched.Append(match)
+	}
+	L.Push(matched)
+	return 1
+}
+
+func replace(L *lua.LState) int {
+	p := L.CheckString(1)
+	s := L.CheckString(2)
+	newStr := L.CheckString(3)
+	pattern, err := regexp.Compile(p)
+	if err != nil {
+		L.Push(lua.LString(s))
+		L.Push(lua.LString(err.Error()))
+		return 2
+	}
+	L.Push(lua.LString(pattern.ReplaceAllString(s, newStr)))
+	return 1
+}
+
+func split(L *lua.LState) int {
+	p := L.CheckString(1)
+	s := L.CheckString(2)
+	n := L.ToInt(3)
+	if n <= 0 {
+		n = -1
+	}
+
+	pattern, err := regexp.Compile(p)
+	if err != nil {
+		L.Push(lua.LNil)
+		L.Push(lua.LString(err.Error()))
+		return 2
+	}
+	table := L.NewTable()
+	for _, item := range pattern.Split(s, n) {
+		table.Append(lua.LString(item))
+	}
+	L.Push(table)
+	return 1
+}
+
 func compile(L *lua.LState) int {
-	pattern := L.CheckString(1)
-	p, err := regexp.Compile(pattern)
+	p := L.CheckString(1)
+	pattern, err := regexp.Compile(p)
 	if err != nil {
 		L.Push(lua.LNil)
 		L.Push(lua.LString(err.Error()))
 		return 2
 	}
 	ud := L.NewUserData()
-	ud.Value = p
+	ud.Value = pattern
 	L.SetMetatable(ud, L.GetTypeMetatable("Regexp"))
 	L.Push(ud)
 	return 1
@@ -66,13 +251,19 @@ func checkRegexp(L *lua.LState) *regexp.Regexp {
 	return nil
 }
 
-func find(L *lua.LState) int {
+func pMatch(L *lua.LState) int {
+	p := checkRegexp(L)
+	L.Push(lua.LBool(p.MatchString(L.CheckString(2))))
+	return 1
+}
+
+func pFind(L *lua.LState) int {
 	p := checkRegexp(L)
 	L.Push(lua.LString(p.FindString(L.CheckString(2))))
 	return 1
 }
 
-func findIndex(L *lua.LState) int {
+func pFindIndex(L *lua.LState) int {
 	p := checkRegexp(L)
 	tmp := p.FindStringIndex(L.CheckString(2))
 	loc := L.NewTable()
@@ -84,7 +275,7 @@ func findIndex(L *lua.LState) int {
 	return 1
 }
 
-func findSubmatch(L *lua.LState) int {
+func pFindSubmatch(L *lua.LState) int {
 	p := checkRegexp(L)
 	tmp := p.FindStringSubmatch(L.CheckString(2))
 	matched := L.NewTable()
@@ -100,7 +291,7 @@ func findSubmatch(L *lua.LState) int {
 	return 1
 }
 
-func findAll(L *lua.LState) int {
+func pFindAll(L *lua.LState) int {
 	p := checkRegexp(L)
 	s := L.CheckString(2)
 	n := L.ToInt(3)
@@ -117,7 +308,7 @@ func findAll(L *lua.LState) int {
 	return 1
 }
 
-func findAllIndex(L *lua.LState) int {
+func pFindAllIndex(L *lua.LState) int {
 	p := checkRegexp(L)
 	s := L.CheckString(2)
 	n := L.ToInt(3)
@@ -136,7 +327,7 @@ func findAllIndex(L *lua.LState) int {
 	return 1
 }
 
-func findAllSubmatch(L *lua.LState) int {
+func pFindAllSubmatch(L *lua.LState) int {
 	p := checkRegexp(L)
 	s := L.CheckString(2)
 	n := L.ToInt(3)
@@ -159,19 +350,13 @@ func findAllSubmatch(L *lua.LState) int {
 	return 1
 }
 
-func pMatch(L *lua.LState) int {
-	p := checkRegexp(L)
-	L.Push(lua.LBool(p.MatchString(L.CheckString(2))))
-	return 1
-}
-
-func replace(L *lua.LState) int {
+func pReplace(L *lua.LState) int {
 	p := checkRegexp(L)
 	L.Push(lua.LString(p.ReplaceAllString(L.CheckString(2), L.CheckString(3))))
 	return 1
 }
 
-func split(L *lua.LState) int {
+func pSplit(L *lua.LState) int {
 	p := checkRegexp(L)
 	s := L.CheckString(2)
 	n := L.ToInt(3)
